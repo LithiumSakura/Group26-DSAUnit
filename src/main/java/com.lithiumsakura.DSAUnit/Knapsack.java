4694054943726dbb1fd5ba5c136dbaa33a57b897
Lisa McKenzie <s5605030@bournemouth.ac.uk>
2025-04-08 12:49:23 +0100

    KSP Start

--- src/main/java/com.lithiumsakura.DSAUnit/Knapsack.java
+++ src/main/java/com.lithiumsakura.DSAUnit/Knapsack.java
@@ -1,5 +1,11 @@
 package com.lithiumsakura.DSAUnit;
 
 public class Knapsack {
-    
+    /* Given a set of items, each with a weight and a value, select a subset of the available items that maximizes 
+    the total value while keeping the total weight below a given carrying capacity. You can choose either the 0-1 
+    knapsack problem or another variant */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
